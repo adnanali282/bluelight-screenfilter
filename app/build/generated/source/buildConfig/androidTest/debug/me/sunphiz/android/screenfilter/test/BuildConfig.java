66fae70fb689d05f1b8ff132188fc0d4286cb7c0
adnan <aliadnan282@gmail.com>
2017-12-20 18:25:45 +0500

    configuring gradle build

---
+++ app/build/generated/source/buildConfig/androidTest/debug/me/sunphiz/android/screenfilter/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.sunphiz.android.screenfilter.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.sunphiz.android.screenfilter.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 49;
+  public static final String VERSION_NAME = "1.0";
+}
